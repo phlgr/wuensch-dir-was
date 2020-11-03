4726e40558a5da66b6fd5fbe87b092c3ecf6a086
phlgr <philippm.gartz@gmail.com>
2020-11-03 09:59:50 +0100

    Create basic routing example

--- src/App.js
+++ src/App.js
@@ -1,68 +1,21 @@
-import styled, { keyframes } from 'styled-components/macro';
-import GlobalStyle from './GlobalStyle';
-import logo from './logo.svg';
-
-const logoSpin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const Wrapper = styled.div`
-  text-align: center;
-
-  header {
-    background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
-  }
-
-  header img {
-    height: 40vmin;
-    pointer-events: none;
-  }
-
-  @media (prefers-reduced-motion: no-preference) {
-    header img {
-      animation: ${logoSpin} infinite 20s linear;
-    }
-  }
-`;
-
-const Link = styled.a`
-  color: #61dafb;
-`;
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <Wrapper>
-        <header>
-          <img src={logo} alt="logo" />
-          <p>
-            Edit
-            <code>src/App.js</code>
-            and save to reload.
-          </p>
-          <Link
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </Link>
-        </header>
-      </Wrapper>
-    </>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/contact">Contact</Route>
+        <Route path="/">Home</Route>
+      </Switch>
+    </Router>
   );
 }
 
