b4a9c1f000f01c6551fddfb4dbf178512f220b2c
phlgr <philippm.gartz@gmail.com>
2020-11-03 13:58:49 +0100

    🎨 Improve indentation

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,9 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-*,*::before,*::after{
-  box-sizing:border-box
-}
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
