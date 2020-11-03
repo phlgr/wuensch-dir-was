417fe7d5b98259fab3d492fb14fac9bc78058a80
phlgr <philippm.gartz@gmail.com>
2020-11-03 11:35:47 +0100

    Create WishListItem Component

---
+++ src/components/WishListItem.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/macro';
+import PropTypes from 'prop-types';
+
+const Container = styled.div`
+  margin: 10px;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 10px 20px lightblue;
+`;
+
+export default function WishListItem({ title }) {
+  return (
+    <>
+      <Container>{title}</Container>
+    </>
+  );
+}
+
+WishListItem.propTypes = {
+  title: PropTypes.string.isRequired,
+};
