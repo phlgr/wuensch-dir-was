affb6decf5a08788ad2d415103e372a836db4367
lmachens <leon.machens@googlemail.com>
2020-11-04 16:39:27 +0100

    Get single list

--- src/pages/WishList.js
+++ src/pages/WishList.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { getListById } from '../api/lists';
 
 const WishList = () => {
   const { listId } = useParams();
+  const [list, setList] = useState(null);
+
+  useEffect(async () => {
+    const newList = await getListById(listId);
+    setList(newList);
+  }, []);
 
   return (
     <div>
-      <Link to="/">Back</Link> {listId} WishList
+      <Link to="/">Back</Link> {list?.title}
+      <ul>
+        {list?.items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 };
